cb9461a760098edd1a264fea056a648e26685af1
Damian <damian.makoza@gmail.com>
2022-08-30 23:01:36 +0200

    First draft of gifs

--- main.go
+++ main.go
@@ -1,19 +1,52 @@
 package main
 
 import (
-	"fmt"
-
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"math/rand"
-
-	algs "github.com/DamianKoz/sorting_algorithms/algorithms"
+	"os"
 )
 
 func main() {
-	// arr := []int{5, 4, 2, 6, 7, 4}
-	arr := initializeArray(1000000, 1000000)
-	fmt.Printf("Unsorted Array: \n %v \n", arr)
-	sortedArray := algs.BubbleSort(arr)
-	fmt.Printf("Sorted Array \n %v", sortedArray)
+	width := 200
+	height := 200
+
+	f, _ := os.Create("test.gif")
+
+	createNewGif(f, width, height)
+
+}
+
+func createNewGif(out io.Writer, width, height int) {
+	images := []*image.Paletted{}
+	for i := 0; i < 50; i++ {
+		img := generateRandomImage(width, height)
+		images := append(images, img)
+	}
+
+	anim := gif.GIF{Delay: []int{0}, Image: images}
+
+	gif.EncodeAll(out, &anim)
+
+}
+
+func generateRandomImage(width, height int) *image.Paletted {
+	palette := []color.Color{color.White, color.Black}
+
+	rect := image.Rect(0, 0, width, height)
+	img := image.NewPaletted(rect, palette)
+
+	for x := 0; x < width; x++ {
+		for y := 0; y < height; y++ {
+			c := rand.Intn(2)
+			img.Set(x, y, palette[c])
+		}
+	}
+
+	img.Set(width/3, height/2, color.Black)
+	return img
 }
 
 func initializeArray(maxNum, length int) []int {
---
+++ main_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestArrayInitilation(t *testing.T) {
+	got := initializeArray(100, 5)
+
+	t.Run("array length", func(t *testing.T) {
+		want := 5
+
+		if len(got) != want {
+			t.Errorf("Got length of %v, Want length of %v", len(got), want)
+		}
+	})
+}
